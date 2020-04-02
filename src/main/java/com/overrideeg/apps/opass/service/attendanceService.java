a0ad3a8593535ff43087590d7be49c450f74ec30
abdelrahman alkholy <alkholyabdelrahman@gmail.com>
2020-04-02 17:59:51 +0200

    custom Query

--- src/main/java/com/overrideeg/apps/opass/service/attendanceService.java
+++ src/main/java/com/overrideeg/apps/opass/service/attendanceService.java
@@ -1,8 +1,17 @@
+/*
+ * Copyright (c) 2020. overrideeg.ocm.
+ */
+
 package com.overrideeg.apps.opass.service;
 
 import com.overrideeg.apps.opass.io.entities.attendance;
 import com.overrideeg.apps.opass.io.repositories.attendanceRepo;
+import com.overrideeg.apps.opass.io.repositories.impl.attendanceRepoImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class attendanceService extends AbstractService<attendance> {
@@ -11,4 +20,12 @@ public class attendanceService extends AbstractService<attendance> {
         super(inRepository);
     }
 
+    @Autowired
+    attendanceRepoImpl attendanceRepo;
+
+    @Override
+    public List<attendance> findAll(int start, int limit) {
+        List<attendance> byDate = attendanceRepo.findByDate(new Date());
+        return super.findAll(start, limit);
+    }
 }
